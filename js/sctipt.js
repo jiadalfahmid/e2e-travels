20a25fde1d692f6e66f4d5a8f5b2f34108fe420b
jiadalfahmid <jiadalfahmid@gmail.com>
2024-09-22 17:15:20 +0600

    Booking restrictions added

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,21 +1,34 @@
-const selectedSeatList = document.getElementById("noSeatSelectedEl");
-let totalSeat = [];
-const seatCount = document.getElementById("seatCount");
-const seatLeftCount = document.getElementById("seatLeftCount");
-const noSeat = document.getElementById('noSeat')
-let totalAmount = 0;
-const totalPrice = document.getElementById("totalPrice");
+   const selectedSeatList = document.getElementById("noSeatSelectedEl");
+   let totalSeat = [];
+   const seatCount = document.getElementById("seatCount");
+   const seatLeftCount = document.getElementById("seatLeftCount");
+   const noSeat = document.getElementById("noSeat");
+   let totalAmount = 0;
+   const totalPrice = document.getElementById("totalPrice");
+   const couponInput = document.getElementById("couponInput");
+   const couponBtn = document.getElementById("couponBtn");
 
-function selectedSeat(event) {
-  event.classList.add("bg-primary", "text-white");
-  let seatNo = event.innerText;
-  totalSeat.push(seatNo);
-  seatCount.innerText = totalSeat.length;
-  seatLeftCount.innerText -= 1;
-  noSeat.classList.add('hidden')
-  selectedSeatList.innerHTML += `
-   <li class="flex justify-between font-normal text base"><span>${seatNo}</span><span>Economy</span><span>550</span></li>
-   `;
-  totalAmount += 550;
-  totalPrice.innerText = totalAmount;
-}
+   function selectedSeat(event) {
+   let seatNo = event.innerText;
+   if (totalSeat.includes(seatNo)) {
+      return alert("Seat has been booked already!!!");
+   }
+   totalSeat.push(seatNo);
+   if (totalSeat.length > 4) {
+      return alert("Maximum Seat has been selected!!!");
+   }
+   event.classList.add("bg-primary", "text-white");
+   seatCount.innerText = totalSeat.length;
+   seatLeftCount.innerText -= 1;
+   noSeat.classList.add("hidden");
+   selectedSeatList.innerHTML += `
+      <li class="flex justify-between font-normal text base"><span></span><span> ${seatNo}</span><span>Economy</span><span>550.00</span></li>
+      `;
+   totalAmount += 550;
+   totalPrice.innerText = totalAmount.toFixed(2);
+
+   if (totalSeat.length == 4) {
+      couponInput.removeAttribute("disabled");
+      couponBtn.removeAttribute("disabled");
+   }
+   }
