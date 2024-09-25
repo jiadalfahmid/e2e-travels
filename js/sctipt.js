4140bb201c28867ca4d1b7c765d3439909ed463c
jiadalfahmid <jiadalfahmid@gmail.com>
2024-09-25 22:29:10 +0600

    Discount Function added

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,34 +1,44 @@
-   const selectedSeatList = document.getElementById("noSeatSelectedEl");
-   let totalSeat = [];
-   const seatCount = document.getElementById("seatCount");
-   const seatLeftCount = document.getElementById("seatLeftCount");
-   const noSeat = document.getElementById("noSeat");
-   let totalAmount = 0;
-   const totalPrice = document.getElementById("totalPrice");
-   const couponInput = document.getElementById("couponInput");
-   const couponBtn = document.getElementById("couponBtn");
+const selectedSeatList = document.getElementById("noSeatSelectedEl");
+let totalSeat = [];
+const seatCount = document.getElementById("seatCount");
+const seatLeftCount = document.getElementById("seatLeftCount");
+const noSeat = document.getElementById("noSeat");
+let totalAmount = 0;
+const totalPrice = document.getElementById("totalPrice");
+const couponInput = document.getElementById("couponInput");
+const couponBtn = document.getElementById("couponBtn");
+let grandTotal = document.getElementById("grandTotal");
 
-   function selectedSeat(event) {
-   let seatNo = event.innerText;
-   if (totalSeat.includes(seatNo)) {
-      return alert("Seat has been booked already!!!");
-   }
-   totalSeat.push(seatNo);
-   if (totalSeat.length > 4) {
-      return alert("Maximum Seat has been selected!!!");
-   }
-   event.classList.add("bg-primary", "text-white");
-   seatCount.innerText = totalSeat.length;
-   seatLeftCount.innerText -= 1;
-   noSeat.classList.add("hidden");
-   selectedSeatList.innerHTML += `
+function selectedSeat(event) {
+  let seatNo = event.innerText;
+  if (totalSeat.includes(seatNo)) {
+    return alert("Seat has been booked already!!!");
+  }
+  totalSeat.push(seatNo);
+  if (totalSeat.length > 4) {
+    return alert("Maximum Seat has been selected!!!");
+  }
+  event.classList.add("bg-primary", "text-white");
+  seatCount.innerText = totalSeat.length;
+  seatLeftCount.innerText -= 1;
+  noSeat.classList.add("hidden");
+  selectedSeatList.innerHTML += `
       <li class="flex justify-between font-normal text base"><span></span><span> ${seatNo}</span><span>Economy</span><span>550.00</span></li>
       `;
-   totalAmount += 550;
-   totalPrice.innerText = totalAmount.toFixed(2);
+  totalAmount += 550;
+  totalPrice.innerText = totalAmount.toFixed(2);
 
-   if (totalSeat.length == 4) {
-      couponInput.removeAttribute("disabled");
-      couponBtn.removeAttribute("disabled");
-   }
-   }
+  if (totalSeat.length == 1) {
+    couponInput.removeAttribute("disabled");
+  }
+}
+
+document.getElementById("couponInput").addEventListener("input", function () {
+  if (couponInput.value === "NEW50" || couponInput.value === "Couple 20") {
+    couponBtn.removeAttribute("disabled");}
+});
+
+couponBtn.addEventListener('click', function(){
+  document.getElementById('couponField').innerHTML = ``
+
+})
